71f317e7e8407cd718f175471834b372ecfea66c
themaheshsutar <maheshsutar9702@outlook.com>
2025-12-31 12:57:47 +0530

    eat: add Player component with name input functionality using useState hook

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,11 +1,25 @@
-export default function Player() {
+import { useState, type ChangeEvent } from "react";
+
+function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSubmitted(false);
+    setEnteredPlayerName(event.target.value);
+  };
+
+  const handleClick = () => setSubmitted(true);
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
 }
+
+export default Player;
